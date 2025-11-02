94ff5c7840547b10fe10e911c9017937779574bd
Kusoro Adeolu <kusoroadeolu@gmail.com>
2025-11-02 14:46:03 +0000

    Remove example Clique code
    Removes placeholder Clique library usage from the `main` method.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import core.clique.Clique;
 
 public class Main {
     public static void main(String[] args) {
-        Clique.parser().print("[blue, bold]Clique is awesome![/]");
-        Clique.enableCliqueColors(false);
-        Clique.parser().print("[blue, bold]Clique is awesome![/]");
 
     }
 }
