221f62f5b0f6b4969e169339cebf6da3633999da
Kusoro Adeolu <kusoroadeolu@gmail.com>
2026-04-08 10:51:43 +0100

    chore: update tests

--- clique-core/src/main/java/module-info.java
+++ clique-core/src/main/java/module-info.java
@@ -2,4 +2,6 @@ module clique.core {
     requires clique.spi;
     requires jdk.sctp;
     uses io.github.kusoroadeolu.clique.spi.CliqueTheme;
+    provides io.github.kusoroadeolu.clique.spi.CliqueTheme
+            with io.github.kusoroadeolu.clique.themeloader.TestTheme;
 }
